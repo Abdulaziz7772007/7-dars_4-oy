66e71b1af2c0e7c124a4b78642a2b55f160cbbf6
Abdulaziz <abdulazizprogrammer777@gmail.com>
2025-08-28 02:08:31 +0500

    ssa

--- js/html-selection.js
+++ js/html-selection.js
@@ -1,15 +1,25 @@
-const handsEL = document.querySelectorAll('.hand')
-const userImg = document.querySelector('#user')
-const chooseZone = document.querySelector('#choose-zone')
-const statusZone = document.querySelector('#status-zone')
-const returnGameBtn = document.querySelector('#refreshGameBtn')
-const aiImg = document.querySelector('.ai')
-const statusText = document.querySelector('.statusText')
-const modeChangerEl = document.getElementById('modeChanger')
-const zoneGridEl = document.getElementById('zoneGrid'),
+const handsEL = document.querySelectorAll('.hand'),
+	userImg = document.querySelector('#user'),
+	chooseZone = document.querySelector('#choose-zone'),
+	statusZone = document.querySelector('#status-zone'),
+	returnGameBtn = document.querySelector('#refreshGameBtn'),
+	aiImg = document.querySelector('.ai'),
+	statusText = document.querySelector('.statusText'),
+	modeChangerEl = document.getElementById('modeChanger'),
+	zoneGridEl = document.getElementById('zoneGrid'),
 	scoreEl = document.getElementById('score'),
 	rulesEl = document.getElementById('rules')
 
-
-
-export { handsEL, userImg, chooseZone, statusZone, returnGameBtn, aiImg, statusText, modeChangerEl, zoneGridEl, scoreEl,rulesEl }
+export {
+	handsEL,
+	userImg,
+	chooseZone,
+	statusZone,
+	returnGameBtn,
+	aiImg,
+	statusText,
+	modeChangerEl,
+	zoneGridEl,
+	scoreEl,
+	rulesEl,
+}
