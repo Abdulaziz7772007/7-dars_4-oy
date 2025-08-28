cf4cccaa49488f9f5ca3220de0e02a191dae8313
Abdulaziz <abdulazizprogrammer777@gmail.com>
2025-08-28 10:19:26 +0500

    responsive

--- js/constants.js
+++ js/constants.js
@@ -10,9 +10,10 @@ export const { rock, scissors, paper, spock, lizard } = {
 export const hands = [rock, scissors, paper, spock, lizard]
 export let mode = zoneGridEl.dataset.mode
 export function modeChanger(value) {
-	mode = value;
+	mode = value
 	zoneGridEl.dataset.mode = value
 }
 
 export const shadows = 'shadow-[0_0_0_20px_rgba(119,130,146,0.2)]'
-export const shadow = 'shadow-[0_0_0_20px_rgba(119,130,146,0.2),0_0_0_40px_rgba(59,130,246,0.15),0_0_0_60px_rgba(59,130,246,0.07)]'
+export const shadow =
+	'shadow-[0_0_0_20px_rgba(119,130,146,0.2),0_0_0_40px_rgba(59,130,246,0.15),0_0_0_60px_rgba(59,130,246,0.07)]'
